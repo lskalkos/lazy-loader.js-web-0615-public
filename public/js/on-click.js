8f5ffe961cf4dca3aeacd347df8cadaba49906e6
Lia Skalkos <hello@liaskalkos.net>
2015-07-21 16:00:52 -0400

    AJAZ lazy load complete

--- public/js/on-click.js
+++ public/js/on-click.js
@@ -3,4 +3,10 @@
 $(document).ready(function() {
   // add click listener here
   // it should call on fetchJSON()
+
+    //var counter = 0;
+
+    $('#load-cars').click(function() {
+        fetchJSON();
+    })
 });
